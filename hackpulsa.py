bd9fbb35c8a7399b1cb31b3632dd394005bdca9d
ellorafin <85016676+ellorafin@users.noreply.github.com>
2023-05-01 14:02:37 +0700

    Hackpulsa

---
+++ hackpulsa.py
@@ -0,0 +1,26 @@
+print ("HACK PULSA BY BGKE04")
+print ("PULSA MURAH TERBARU 2023")
+print ("")
+no = input("NO TELEPHONE:")
+print ("")
+toy = print ("WAITING")
+import time
+time.sleep(1)
+print ("SUCCES HACKING PULSA NOMOR ",no)
+print ("")
+
+print ("1.PULSA 50k RIBU = 10K")
+print ("2.PULSA 100K RIBU = 20K")
+
+hack = int(input("PILIH:"))
+if hack >1:
+	time.sleep(1)
+	print ("UNTUK PULSA 100K SILAHKAN TF NO DI BAWAH INI SEHARGA 20K:")
+else:
+	time.sleep(1)
+	print ("UNTUK PULSA 50K SILAHKAN TF NO DI BAWAH INI SEHARGA 10K:")
+time.sleep(1)
+print ("083194920431")
+time.sleep(1)
+print ("PEMBAYARAN BISA VIA: DANA,GOPAY,PULSA")
+
